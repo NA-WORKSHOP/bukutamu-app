c228508d3fbce8e1286aa49906691e357026323e
Fauzun Naja <70993035+Funazu@users.noreply.github.com>
2023-05-26 11:04:05 +0700

    create database bukutamu

--- database/migrations/2023_05_26_035556_create_buku_tamus_table.php
+++ database/migrations/2023_05_26_035556_create_buku_tamus_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('buku_tamus', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('password');
+            $table->string('nama');
+            $table->string('waktu');
+            $table->string('asal');
+            $table->string('keterangan');
+            $table->string('kontak');
             $table->timestamps();
         });
     }
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('buku_tamus');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\BukuTamu;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        BukuTamu::create([
+            'nama' => "Lia",
+            'waktu' => "Jumat, 26 Mei 2023",
+            'asal' => "Kudus",
+            'keterangan' => "Konsultasi WIFI",
+            'kontak' => "08xxxxxx"
+        ]);
     }
+    
 }
